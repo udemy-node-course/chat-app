cd081c0364bdcbbaee669e0d4150ea199cfd90dc
sgigli <simon.luciano@gigli.net>
2020-05-22 16:38:37 -0400

    Init commit; in process of testing socket.io

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const http = require('http')
+const path = require('path')
+const socketio = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection')
+
+    // socket.emit('countUpdated', count)
+
+    // socket.on('increment', () => {
+    //     count++
+    //     io.emit('countUpdated', count)
+    // })
+})
+
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}!`)
+})
