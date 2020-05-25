819fdafa44c2a5c918545b76763c68729ae74121
sgigli <simon.luciano@gigli.net>
2020-05-25 15:06:21 -0400

    Real time messages appearing

--- public/js/chat.js
+++ public/js/chat.js
@@ -8,3 +8,13 @@ const socket = io()
 //     console.log('Clicked')
 //     socket.emit('increment')
 // })
+
+socket.on('message', (message) => {
+    console.log(message)
+})
+
+document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const message = document.querySelector('input').value
+    socket.emit('sendMessage', message)
+})
--- src/index.js
+++ src/index.js
@@ -15,6 +15,12 @@ app.use(express.static(publicDirectoryPath))
 io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
+    socket.emit('message', 'Welcome!')
+
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message)
+    })
+
     // socket.emit('countUpdated', count)
 
     // socket.on('increment', () => {
